b64aaf3f13559df5f438385117cd3311e8c50001
Vedant <vedantulhaspandit@gmail.com>
2016-10-14 08:06:37 -0700

    first commit

---
+++ app/src/main/java/com/example/vedant/facebooklogin/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.vedant.facebooklogin;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    Uri imageuri;
+    AccessTokenTracker accessTokenTracker;
+    String username, firstname, lastname;
+    TextView tv_profile_name;
+    ImageView iv_profile_pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.vedant.facebooklogin",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        tv_profile_name = (TextView) findViewById(R.id.tv_profile_name);
+        iv_profile_pic = (ImageView) findViewById(R.id.iv_profile_pic);
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+
+                                try {
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday");
+                                    String id = object.getString("id");
+                                    String name = object.getString("name");
+                                    tv_profile_name.setText(name);
+
+
+                                    String imageurl = "https://graph.facebook.com/" + id + "/picture?type=large";
+
+                                    Picasso.with(MainActivity.this).load(imageurl).into(iv_profile_pic);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+/**
+ * AccessTokenTracker to manage logout
+ */
+                accessTokenTracker = new AccessTokenTracker() {
+                    @Override
+                    protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                               AccessToken currentAccessToken) {
+                        if (currentAccessToken == null) {
+                            tv_profile_name.setText("");
+                            //iv_profile_pic.setImageResource(R.drawable.maleicon);
+                        }
+                    }
+                };
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
